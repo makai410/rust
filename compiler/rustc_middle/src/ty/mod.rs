0064ddf1903d9cfe50b6d5f4735a5341e91c71ff
Michael Goulet <michael@errs.io>
2025-03-13 03:42:06 +0000

    Add clauses to binders, once again

--- compiler/rustc_middle/src/ty/mod.rs
+++ compiler/rustc_middle/src/ty/mod.rs
@@ -1154,7 +1154,7 @@ pub struct ParamEnv<'tcx> {
 }
 
 impl<'tcx> rustc_type_ir::inherent::ParamEnv<TyCtxt<'tcx>> for ParamEnv<'tcx> {
-    fn caller_bounds(self) -> impl inherent::SliceLike<Item = ty::Clause<'tcx>> {
+    fn caller_bounds(self) -> Clauses<'tcx> {
         self.caller_bounds()
     }
 }
